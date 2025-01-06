87fcc6285bb299aba0378a97be577ec9444c5b02
Renato SICLER <renatosicler@gmail.com>
2025-01-06 11:09:21 +0100

    Add package nugets and create DbContext with EFCore

---
+++ PatientsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patient.Domain;
+
+namespace Patient.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patients> Patients { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patients>()
+                .HasOne(p => p.Genre) 
+                .WithMany(g => g.Patients) 
+                .HasForeignKey(p => p.GenreId); 
+        }
+    }
+}
