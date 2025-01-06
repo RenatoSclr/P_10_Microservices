c5dead723612d4256e61050b36cc4393027bd080
Renato SICLER <renatosicler@gmail.com>
2025-01-06 11:12:18 +0100

    Add PatientRepository

---
+++ PatientsAPI/Domain/IRepository/IPatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Patient.Domain;
+
+namespace Patient.Domain.IRepository
+{
+    public interface IPatientRepository
+    {
+        Task<List<Patients>> GetAllPatients();
+        Task<Patients> GetPatients(int id);
+        Task AddPatient(Patients patient);
+        Task UpdatePatient(Patients patient);
+        Task DeletePatient(Patients patient);
+        Task Save();
+    }
+}
---
+++ PatientsAPI/Repository/PatientRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patient.Data;
+using Patient.Domain;
+using Patient.Domain.IRepository;
+
+namespace Patient.Repository
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddPatient(Patients patients)
+        {
+            await _context.Patients.AddAsync(patients);
+        }
+
+        public async Task DeletePatient(Patients patient)
+        {
+            _context.Remove(patient);
+        }
+
+        public async Task<List<Patients>> GetAllPatients()
+        {
+            return await _context.Patients.ToListAsync();
+        }
+
+        public async Task<Patients> GetPatients(int id)
+        {
+            return await _context.Patients.FindAsync(id);
+        }
+
+        public async Task UpdatePatient(Patients patients)
+        {
+            _context.Update(patients);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
