815df3e196989d6fc01c925597dd5db7e05e496c
Renato SICLER <renatosicler@gmail.com>
2025-01-06 11:01:38 +0100

    Create Domain for Patients

---
+++ PatientsAPI/Domain/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace Patient.Domain
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string GenreLabel { get; set; }
+        public ICollection<Patients> Patients { get; set; }
+    }
+}
---
+++ PatientsAPI/Domain/Patients.cs
@@ -0,0 +1,15 @@
+﻿namespace Patient.Domain
+{
+    public class Patients
+    {
+        public Guid PatientId { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        public string NumeroTelephone { get; set; }
+    }
+}
