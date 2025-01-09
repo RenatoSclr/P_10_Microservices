0b186cc276a606a63b9639fc21a23a19d6783c95
Renato SICLER <renatosicler@gmail.com>
2025-01-09 16:32:31 +0100

    Add Identity project

---
+++ Identity/Data/IdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Data
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
