08f2a146fa4f6a21b37934f845d625cf08113f3b
Renato SICLER <renatosicler33@gmail.com>
2025-01-20 11:57:52 +0100

    Add test for borderline detection

--- DiabeticAssessmentAPI/Services/DiabetesReportService.cs
+++ DiabeticAssessmentAPI/Services/DiabetesReportService.cs
@@ -8,13 +8,19 @@ namespace DiabeticAssessmentAPI.Services
     {
         public string GetDiabeteReportByPatientId(InfoPatientDTO infoPatient, List<ContenuNotePatientDTO> contenuNoteDTOs)
         {
+            DateTime dateNow = DateTime.Now;
+            int age = dateNow.Year - infoPatient.DateNaissance.Year;
+            if (infoPatient.DateNaissance > dateNow.AddYears(-age))
+                age--;
+
             int count = contenuNoteDTOs.Count(note => Declencheurs.Liste.Any(d =>
             note.Contenu.Contains(d, StringComparison.OrdinalIgnoreCase)));
 
             if (count == 0)
-            {
                 return "None";
-            }
+
+            if (age >= 30 && 2 <= count && count <= 5)
+                return "Borderline";
 
             return "À implémenter";
         }
--- DiabeticAssessmentAPITests/UnitTests/DiabetesReportServiceTests.cs
+++ DiabeticAssessmentAPITests/UnitTests/DiabetesReportServiceTests.cs
@@ -30,5 +30,33 @@ namespace DiabeticAssessmentAPITests.UnitTests
             //Assert
             Assert.Equal(result.ToLower(), expected.ToLower());
         }
+
+        [Fact]
+        public async Task Should_return_Borderline_When_ContenuNotePatientDTO_Contains_Between_Two_and_Five_Triggers_and_AgePatient_greater_than_thirty()
+        {
+            var expected = "Borderline";
+
+            //Arrange
+            var diabeteReportService = new DiabetesReportService();
+            InfoPatientDTO infoPatientDTO = new InfoPatientDTO
+            {
+                DateNaissance = new DateTime(1945, 05, 24),
+                Genre = "Homme"
+            };
+
+            List<ContenuNotePatientDTO> contenuNoteDTOs = new List<ContenuNotePatientDTO>
+            {
+                new ContenuNotePatientDTO { Contenu = "Le patient déclare qu'il ressent beaucoup de stress au travail Il se plaint également que son audition est anormale dernièrement" },
+                new ContenuNotePatientDTO { Contenu = "Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale" }
+            };
+
+
+            //Act
+            var result = diabeteReportService.GetDiabeteReportByPatientId(infoPatientDTO, contenuNoteDTOs);
+
+
+            //Assert
+            Assert.Equal(result.ToLower(), expected.ToLower());
+        }
     }
 }
