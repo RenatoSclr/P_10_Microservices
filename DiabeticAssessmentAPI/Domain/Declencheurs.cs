1b03eb53fa295213cda658b43998670094c7334c
Renato SICLER <renatosicler33@gmail.com>
2025-01-21 12:17:49 +0100

    add diabetes report to view

--- DiabeticAssessmentAPI/Domain/Declencheurs.cs
+++ DiabeticAssessmentAPI/Domain/Declencheurs.cs
@@ -3,10 +3,10 @@
     public class Declencheurs
     {
         public static readonly List<string> Liste = new()
-    {
-        "Hémoglobine A1C", "Microalbumine", "Taille", "Poids",
-        "Fumeur", "Fumeuse", "Anormal", "Cholestérol",
-        "Vertiges", "Rechute", "Réaction", "Anticorps"
-    };
+        {
+            "Hémoglobine A1C", "Microalbumine", "Taille", "Poids",
+            "Fumeur", "Fumeuse", "Anormal", "Cholestérol",
+            "Vertiges", "Rechute", "Réaction", "Anticorps"
+        };
     }
 }
--- Frontend/ViewModel/DiabeteViewModel/ReportDiabeteViewModel.cs
+++ Frontend/ViewModel/DiabeteViewModel/ReportDiabeteViewModel.cs
@@ -2,5 +2,7 @@
 {
     public class ReportDiabeteViewModel
     {
+        public string NiveauRisque { get; set; }
+        public List<string> Declencheurs { get; set; }
     }
 }
